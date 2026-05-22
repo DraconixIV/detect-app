1255e99dbcb2ee633e66367fcb3791330db5370d
DraconixIV <dblue3642@gmail.com>
2026-05-22 19:40:32 +0200

    fix vite ssl

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,58 @@
 import { defineConfig } from "vite";
-
 import react from "@vitejs/plugin-react";
-
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
 
-    basicSsl()
+    VitePWA({
+      registerType: "autoUpdate",
+
+      includeAssets: [
+        "favicon.svg",
+        "icon-192.png",
+        "icon-512.png"
+      ],
+
+      manifest: {
+        name: "Metal Detector App",
+
+        short_name: "Detector",
+
+        description:
+          "Application de détection métal",
+
+        theme_color: "#1f2937",
+
+        background_color: "#111827",
+
+        display: "standalone",
+
+        orientation: "portrait",
+
+        scope: "/",
+
+        start_url: "/",
+
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
   ],
 
   server: {
-    host: true,
-    https: true
+    host: true
   }
 });
