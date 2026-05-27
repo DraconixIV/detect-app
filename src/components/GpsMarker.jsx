113d69ba79a44b2b47a6abd24f6088a0e5573e26
DraconixIV <dblue3642@gmail.com>
2026-05-27 16:34:01 +0200

    fix gps marker

--- src/components/GpsMarker.jsx
+++ src/components/GpsMarker.jsx
@@ -1,4 +1,30 @@
-import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
+import {
+  Marker,
+  Popup
+} from "react-leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+/* FIX ICONE LEAFLET VITE */
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    markerIcon2x,
+
+  iconUrl:
+    markerIcon,
+
+  shadowUrl:
+    markerShadow
+});
 
 export default function GpsMarker({
   position
