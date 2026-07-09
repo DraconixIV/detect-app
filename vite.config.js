b5e34d22b8d88a4f688b0f29e41900a78cbe3ddc
Leon <dblue3642@gmail.com>
2026-07-09 17:18:24 +0200

    perf: configuration de la limite de taille des chunks et manualChunks dans vite.config.js pour supprimer les warnings et optimiser le cache de production

--- vite.config.js
+++ vite.config.js
@@ -101,5 +101,18 @@ export default defineConfig({
 
   server: {
     host: true
+  },
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        }
+      }
+    }
   }
 });
